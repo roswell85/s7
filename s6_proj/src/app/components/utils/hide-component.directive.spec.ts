ecbbfe661460e2669c06df74a4234f7aaa0740c8
ali <alishawkii885@gmail.com>
2023-02-28 03:05:59 -0800

    commit 2

---
+++ s6_proj/src/app/components/utils/hide-component.directive.spec.ts
@@ -0,0 +1,9 @@
+import { HideComponentDirective } from './hide-component.directive';
+
+describe('HideComponentDirective', () => {
+  it('should create an instance', () => {
+    // @ts-ignore
+    const directive = new HideComponentDirective();
+    expect(directive).toBeTruthy();
+  });
+});
